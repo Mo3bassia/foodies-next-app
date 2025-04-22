0a63d963b4cfc93007313a9a50f19bdcd3977488
Mohamed Abassia <mo3bassia@gmail.com>
2025-04-22 18:11:53 +0200

    add error page in if database is offline or failed

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+"use client";
+export default function error({ error }) {
+  console.log(error);
+  return (
+    <main className="error">
+      <h1>An error occured!</h1>
+      <p>Error is {`"${error.message}"`}</p>
+    </main>
+  );
+}
