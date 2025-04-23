7ec19dd59ff0bb9b827fca91cc44b3b3000a34f4
Mohamed Abassia <mo3bassia@gmail.com>
2025-04-23 13:07:31 +0200

    add share meal action in sepearte file to be able to get share meal page client component

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,21 +1,9 @@
+"use client";
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
+import { shareMeal } from "@/lib/actions";
 
 export default function ShareMealPage() {
-  async function shareMeal(formData) {
-    "use server";
-    console.log("Hello");
-    const meal = {
-      title: formData.get("title"),
-      instructions: formData.get("instructions"),
-      summary: formData.get("summary"),
-      image: formData.get("image"),
-      creator: formData.get("name"),
-      creator_email: formData.get("email"),
-    };
-    console.log(meal);
-  }
-
   return (
     <>
       <header className={classes.header}>
